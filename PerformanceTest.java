086971f87050d883449240efae9fbd238267f91c
Karan M <karanmoods@gmail.com>
2026-04-19 10:21:15 +0530

    Add files via upload

---
+++ PerformanceTest.java
@@ -0,0 +1,24 @@
+package A2;
+public class PerformanceTest {
+    public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
+        // StringBuffer Test
+        StringBuffer sb = new StringBuffer("Java");
+        for (int i = 0; i < 10000; i++) {
+            sb.append("Tpoint");
+        }
+        System.out.println("Time taken by StringBuffer: " 
+                + (System.currentTimeMillis() - startTime) + " ms");
+
+        // StringBuilder Test
+        startTime = System.currentTimeMillis();
+        StringBuilder sb2 = new StringBuilder("Java");
+        for (int i = 0; i < 10000; i++) {
+            sb2.append("Tpoint");
+        }
+        System.out.println("Time taken by StringBuilder: " 
+                + (System.currentTimeMillis() - startTime) + " ms");
+    }
+}
